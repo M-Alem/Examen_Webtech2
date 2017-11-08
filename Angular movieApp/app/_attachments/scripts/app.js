59f3c19d30a1e6b8d666fd6d2c9a3183813e3b92
M-Alem <32704888+M-Alem@users.noreply.github.com>
2017-11-08 16:24:50 +0100

    controllers en services bijgezet

--- Angular movieApp/app/_attachments/scripts/app.js
+++ Angular movieApp/app/_attachments/scripts/app.js
@@ -7,7 +7,7 @@ app.config(function($routeProvider) {
                 controller: 'homeCtrl'
             })
             .when('/actor', {
-                templateUrl: 'assets/views/persons.html',
+                templateUrl: 'assets/views/actor.html',
                 controller: 'actorCtrl'
             })
             .otherwise({
@@ -15,4 +15,40 @@ app.config(function($routeProvider) {
             });
     })
     
-	app.controller('MainController', ['$scope', function($scope) {}])
+    
+	app.controller('homrCtrl', ['$scope', function($scope, filmSrv) {
+		$('#searchButton').on('click', function(e) {
+			
+			var actor = $('#actorText').val();
+			$scope.movies = filmSrv.getMovies(actor);
+
+		})
+	}])
+	
+	app.controller('actorCtrl', ['$scope', function($scope) {}])
+	
+	
+	 app.service('filmSrv', function($http, $q) {
+    		his.getMovies = function(actor) {
+	    		var q = $q.defer();
+	    		var url = 'http://www.theimdbapi.org/api/find/person?name=' + encodeURIComponent(actor) + '&format=json';
+	    	var movies[{
+	    		var list[];
+	            for (var j = 0; j < movies.length; j++) {
+	                 if (movies[j].name === actor.name) {
+	                     list.push(movie[j]);
+	                 }
+	            }*/
+	            return list;
+	    	}]
+
+	    		$http.get(url)
+	    			.then(function(data){
+	    				q.resolve(data);
+	    			}, function error(err) {
+	    				q.reject(err);
+	    			});
+	    			
+	    			return q.promise;
+	    		};
+    });
